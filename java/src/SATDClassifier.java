0c9d3c8778f615ee5656d846a5a18976852d4756
wahidnurhani <wahidnurhani@googlemail.com>
2021-08-23 00:07:28 +0200

    data cleaning : strip, lower, remove html-tag and url

--- java/src/SATDClassifier.java
+++ java/src/SATDClassifier.java
@@ -13,7 +13,7 @@ public class SATDClassifier {
     private static Runtime runtime;
     private static String where = "";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         if (args.length > 0) {
             where = args[0] + File.separator;
         }
@@ -23,54 +23,45 @@ public class SATDClassifier {
         Scanner in = new Scanner(System.in);
         int cv_number = in.nextInt();
         if(cv_number>=1 && cv_number<=10){
-            classify(cv_number);
+            int py_process = runPreprocessor(cv_number);
+            if(py_process==220){
+                classify();
+            }
         } else {
             System.out.println("pleas type number between 1 and 10");
         }
     }
 
-    public static void classify(int cv_number){
+    public static void classify(){
         Thread thread = new Thread(() -> {
             try {
-                runPreprocessor(cv_number);
                 System.out.println();
                 System.out.println("Training ColumnDataClassifier");
                 ColumnDataClassifier cdc = new ColumnDataClassifier(where + ClassifierConstant.propFilePath);
+                cdc.trainClassifier(where + ClassifierConstant.trainFilePath);
 
-                //cdc.trainClassifier(where + ClassifierConstant.trainFilePath);
-            } catch (IOException | InterruptedException e) {
+                System.out.println();
+                System.out.println("Testing predictions of ColumnDataClassifier");
+                for (String line : ObjectBank.getLineIterator(where + ClassifierConstant.testFilePath, "utf-8")) {
+                    // instead of the method in the line below, if you have the individual elements
+                    // already you can use cdc.makeDatumFromStrings(String[])
+                    Datum<String,String> d = cdc.makeDatumFromLine(line);
+                    System.out.printf("%s  ==>  %s (%.4f)%n", line, cdc.classOf(d), cdc.scoresOf(d).getCount(cdc.classOf(d)));
+                }
+
+                System.out.println();
+                System.out.println("Testing accuracy of ColumnDataClassifier");
+                Pair<Double, Double> performance = cdc.testClassifier(where + ClassifierConstant.testFilePath);
+                System.out.printf("Accuracy: %.3f; macro-F1: %.3f%n", performance.first(), performance.second());
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
-
-//
-//                System.out.println();
-//                System.out.println("Testing predictions of ColumnDataClassifier");
-//                for (String line : ObjectBank.getLineIterator(where + ClassifierConstant.testFilePath, "utf-8")) {
-//                    // instead of the method in the line below, if you have the individual elements
-//                    // already you can use cdc.makeDatumFromStrings(String[])
-//                    Datum<String,String> d = cdc.makeDatumFromLine(line);
-//                    System.out.printf("%s  ==>  %s (%.4f)%n", line, cdc.classOf(d), cdc.scoresOf(d).getCount(cdc.classOf(d)));
-//                }
-//
-//                System.out.println();
-//                System.out.println("Testing accuracy of ColumnDataClassifier");
-//                Pair<Double, Double> performance = cdc.testClassifier(where + "data/examples/iris.test");
-//                System.out.printf("Accuracy: %.3f; macro-F1: %.3f%n", performance.first(), performance.second());
         });
         thread.start();
-
-
-
-
-
-
-
-
-
     }
 
-    public static void runPreprocessor(int crossValidationNumber) throws IOException, InterruptedException {
+    public static int runPreprocessor(int crossValidationNumber) throws IOException, InterruptedException {
         String csv_source = "data/src/technical_debt_dataset.csv";
         String[] cmd = {
                 "python",
@@ -96,5 +87,6 @@ public class SATDClassifier {
         System.out.println(" \n" + buffer);
         System.out.println("Process exit value:"+exitCode);
         br.close();
+        return exitCode;
     }
 }
--- python/src/preprocessor.py
+++ python/src/preprocessor.py
@@ -2,6 +2,106 @@ import sys
 
 import numpy as np
 import pandas as pd
+import re
+from bs4 import BeautifulSoup
+# from nltk.corpus import stopwords
+#  from nltk.stem.snowball import SnowballStemmer
+# from autocorrect import Speller
+
+# from nltk.stem import WordNetLemmatizer
+
+# stop_words = stopwords.words('english')
+# lemma = WordNetLemmatizer()
+# stemmer = SnowballStemmer("english")
+
+
+# spell = Speller(lang='en')
+
+
+def remove_html_tags(text_data):
+    soup = BeautifulSoup(text_data, 'lxml')
+    return soup.get_text().strip()
+
+
+def remove_url(text_data):
+    re_https = re.sub(r"http\S+", "", text_data)
+    re_www = re.sub(r"www.\S+", "", re_https)
+    return re_www.strip()
+
+
+# def remove_special_character(phrase, remove_number=False):
+#     phrase = re.sub("\S*\d\S*", "", phrase).strip()
+#     if remove_number:
+#         phrase = re.sub('[^A-Za-z]+', ' ', phrase)
+#     else:
+#         phrase = re.sub('[^A-Za-z0-9]+', ' ', phrase)
+#     return phrase.strip()
+#
+#
+# # def spelling_corrector(text_data):
+# #     words = text_data.split()
+# #     result = ""
+# #     for w in words:
+# #         if w not in stop_words:
+# #             result = result + " " + spell(w)
+# #     return result.strip()
+#
+#
+# def remove_stopwords(text_data):
+#     words = text_data.split()
+#     result = ""
+#     for w in words:
+#         if w not in stop_words:
+#             result = result + " " + w
+#     return result.strip()
+#
+#
+# # def stemming_text(text_data):
+# #     words = text_data.split()
+# #     result = ""
+# #     for w in words:
+# #         result = result + " " + stemmer.stem(w)
+# #     return result.strip()
+#
+#
+# # def lem_text(text_data):
+# #     words = text_data.split()
+# #     result = ""
+# #     for w in words:
+# #         result = result + " " + lemma.lemmatize(word=w, pos='v')
+# #     return result.strip()
+#
+#
+def fix_contractions(phrase):
+    # specific
+    phrase = re.sub(r"won\'t", "will not", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+
+    # general
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    return phrase
+
+
+# def replace_empty_string(text_data):
+#     if len(text_data.strip()) < 1:
+#         return np.nan
+#     return text_data.strip()
+#
+#
+# def remove_unwanted_words(text_data):
+#     words = text_data.split()
+#     result = ""
+#     for w in words:
+#         if len(w) > 2:
+#             result = result + " " + w
+#     return result
 
 
 def export_to_train_and_test(df1, df2, train_path, test_path):
@@ -11,9 +111,21 @@ def export_to_train_and_test(df1, df2, train_path, test_path):
 
 def format_data(data_frame: pd.DataFrame):
     cols_to_check = ['commenttext']
+    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: x.strip())
+    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: x.lower())
     data_frame[cols_to_check] = data_frame[cols_to_check].replace({'\t': ' '}, regex=True)
-    # data_frame[cols_to_check] = data_frame[cols_to_check].replace({'\n': ' '}, regex=True)
-    return data_frame
+    data_frame[cols_to_check] = data_frame[cols_to_check].replace({'\n': '. '}, regex=True)
+    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: remove_html_tags(x))
+    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: remove_url(x))
+    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: fix_contractions(x))
+    # data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: remove_stopwords(x))
+    # data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: remove_unwanted_words(x))
+    # # data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: stemming_text(x))
+    # # data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: lem_text(x))
+    # data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: remove_special_character(x))
+    # data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: replace_empty_string(x))
+    data_frame_clean = df.drop_duplicates(subset=['classification', 'commenttext'], keep='last')
+    return data_frame_clean.dropna()
 
 
 def split_data(data_number: int, data_frame):
@@ -25,8 +137,8 @@ def split_data(data_number: int, data_frame):
     return train, test
 
 
-def transform_data(data_number, df, train_path, test_path):
-    train, test = split_data(data_number, df)
+def transform_data(data_number, _df, train_path, test_path):
+    train, test = split_data(data_number, _df)
     export_to_train_and_test(train, test, train_path, test_path)
 
 
