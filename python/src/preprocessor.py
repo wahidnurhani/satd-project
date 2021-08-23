62bfa37a2cc379cbe1c8650843bfc16f2442bca0
wahidnurhani <wahidnurhani@googlemail.com>
2021-08-23 20:28:37 +0200

    finalizing

--- python/src/preprocessor.py
+++ python/src/preprocessor.py
@@ -3,60 +3,17 @@ import sys
 import numpy as np
 import pandas as pd
 import re
+import wordninja
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
-
+from nltk.stem import WordNetLemmatizer
+from spellchecker import SpellChecker
 
 stop_words = stopwords.words('english')
-stemmer = SnowballStemmer("english")
+lemma = WordNetLemmatizer
+spell = SpellChecker()
 
 
-def remove_html_tags(text_data):
-    soup = BeautifulSoup(text_data, 'lxml')
-    return soup.get_text().strip()
-
-
-def remove_url(text_data):
-    re_https = re.sub(r"http\S+", "", text_data)
-    re_www = re.sub(r"www.\S+", "", re_https)
-    return re_www.strip()
-
-
-def remove_special_character(phrase, remove_number=False):
-    phrase = re.sub("\S*\d\S*", "", phrase).strip()
-    if remove_number:
-        phrase = re.sub('[^A-Za-z]+', ' ', phrase)
-    else:
-        phrase = re.sub('[^A-Za-z0-9]+', ' ', phrase)
-    return phrase.strip()
-
-
-def remove_stopwords(text_data):
-    words = text_data.split()
-    result = ""
-    for w in words:
-        if w not in stop_words:
-            result = result + " " + w
-    return result.strip()
-
-
-def stemming_text(text_data):
-    words = text_data.split()
-    result = ""
-    for w in words:
-        result = result + " " + stemmer.stem(w)
-    return result.strip()
-
-
-# # def lem_text(text_data):
-# #     words = text_data.split()
-# #     result = ""
-# #     for w in words:
-# #         result = result + " " + lemma.lemmatize(word=w, pos='v')
-# #     return result.strip()
-#
-#
 def fix_contractions(phrase):
     # specific
     phrase = re.sub(r"won\'t", "will not", phrase)
@@ -80,12 +37,63 @@ def replace_empty_string(text_data):
     return text_data.strip()
 
 
-def remove_unwanted_words(text_data):
+def remove_stopwords_and_lemmatization(text_data):
     words = text_data.split()
     result = ""
     for w in words:
-        if len(w) > 2:
-            result = result + " " + w
+        if w not in stop_words:
+            if len(w) > 2:
+                result = result + " " + lemma.lemmatize('v', w)
+    return result.strip()
+
+
+def initial_cleaning_and_fix_contractions(text_data):
+    # Stanford classifier form
+    text_data = text_data.replace('\t', ' ')
+    text_data = text_data.replace('\n', '. ')
+
+    # remove html tags and url
+    soup = BeautifulSoup(text_data, 'lxml')
+    re_https = re.sub(r"http\S+", "", soup.get_text())
+    clean_text = re.sub(r"www.\S+", "", re_https)
+
+    contractions_free = fix_contractions(clean_text)
+    return contractions_free
+
+
+# def split_concatenated_word(text_data):
+#     words = text_data.split()
+#     result = ""
+#     for w in words:
+#         splitted = wordninja.split(w)
+#         for s in splitted:
+#             result = result + " " + s
+#     return result.strip()
+
+
+def second_cleaning_and_lemmatization(phrase, remove_number=False):
+    # remove whitespaces and wordnumber
+    phrase = re.sub("\S*\d\S*", "", phrase).strip()
+    # remove unwanted character
+    if remove_number:
+        phrase = re.sub('[^A-Za-z]+', ' ', phrase)
+    else:
+        phrase = re.sub('[^,.A-Za-z0-9]+', ' ', phrase)
+    if len(phrase) <= 2:
+        phrase = ""
+    else:
+        phrase = remove_stopwords_and_lemmatization(phrase)
+
+    # replace empty string with np.nan
+    phrase = replace_empty_string(phrase)
+    return phrase
+
+
+def spell_check(text_data):
+    words = text_data.split()
+    result = ""
+    for w in words:
+        result = result + " " + spell.correction(w)
     return result
 
 
@@ -96,19 +104,11 @@ def export_to_train_and_test(df1, df2, train_path, test_path):
 
 def clean_and_normalize_data(data_frame: pd.DataFrame):
     cols_to_check = ['commenttext']
-    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: x.strip())
     data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: x.lower())
-    data_frame[cols_to_check] = data_frame[cols_to_check].replace({'\t': ' '}, regex=True)
-    data_frame[cols_to_check] = data_frame[cols_to_check].replace({'\n': '. '}, regex=True)
-    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: remove_html_tags(x))
-    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: remove_url(x))
-    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: fix_contractions(x))
-    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: remove_stopwords(x))
-    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: remove_special_character(x))
-    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: remove_unwanted_words(x))
-    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: stemming_text(x))
-    # # data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: lem_text(x))
-    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: replace_empty_string(x))
+    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: initial_cleaning_and_fix_contractions(x))
+    # data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: split_concatenated_word(x))
+    # data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: spell_check(x))
+    data_frame[cols_to_check] = data_frame[cols_to_check].applymap(lambda x: second_cleaning_and_lemmatization(x))
     data_frame_clean = df.drop_duplicates(subset=['classification', 'commenttext'], keep='last')
     return data_frame_clean.dropna()
 
