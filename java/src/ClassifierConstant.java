ad12acb50193f7e2eeb1364adba3ff6f7dc358da
wahidnurhani <wahidnurhani@googlemail.com>
2021-08-26 12:45:33 +0200

    file-path for windows 2

--- java/src/ClassifierConstant.java
+++ java/src/ClassifierConstant.java
@@ -1,9 +1,9 @@
 public class ClassifierConstant {
     public class Unix {
-        public static final String pythonPath = "python/src/preprocessor.py";
-        public static final String csv_source = "data/src/technical_debt_dataset.csv";
-        public static final String testFilePath = "data/splited/testFile.test";
-        public static final String trainFilePath = "data/splited/trainFile.train";
-        public static final String propFilePath = "data/splited/propFile.prop";
+        public static final String pythonPath = "./python/src/preprocessor.py";
+        public static final String csv_source = "./data/src/technical_debt_dataset.csv";
+        public static final String testFilePath = "./data/splited/testFile.test";
+        public static final String trainFilePath = "./data/splited/trainFile.train";
+        public static final String propFilePath = "./data/splited/propFile.prop";
     }
 }
